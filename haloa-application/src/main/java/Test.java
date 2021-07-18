ff3fe788366a6d0c8b88bf3d5c22d4319f7987fe
lv <lvbwei@163.com>
2021-07-18 10:27:13 +0800

    第一次提交

---
+++ haloa-application/src/main/java/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Test.java
+ * Copyright 2021 HelloBike , all rights reserved.
+ * HelloBike PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
+ */
+
+/**
+ * @author lvbenwei11319@hellobike.com
+ * @date 2021/7/18
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
